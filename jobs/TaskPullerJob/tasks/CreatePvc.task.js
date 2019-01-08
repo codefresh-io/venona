c9730431b28dd2a2cb749206570ba416435fca4a
Itai Gendler <itai@codefresh.io>
2019-01-09 01:48:07 +0200

    add tasks for create and delete pvc

---
+++ jobs/TaskPullerJob/tasks/CreatePvc.task.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const Base = require('../../BaseJob');
+
+const ERROR_MESSAGES = {
+	FAILED_TO_EXECUTE_TASK: 'Failed to run task CreatePvc',
+};
+
+class CreatePvcTask extends Base {
+	async run(task) {
+		this.logger.info('Running CreatePvc task');
+		try {
+			const pvc = await this.kubernetesAPI.createPvc(this.logger, task.spec);
+			return pvc;
+		} catch (err) {
+			const message = `${ERROR_MESSAGES.FAILED_TO_EXECUTE_TASK}: ${err.message}`;
+			this.logger.error(message);
+			throw new Error(message);
+		}
+	}
+
+	async validate(task) {
+		return Joi.validate(task, CreatePvcTask.validationSchema);
+	}
+}
+CreatePvcTask.Errors = ERROR_MESSAGES;
+CreatePvcTask.validationSchema = Joi.object().keys({
+	spec: Joi.object().required(),
+}).options({ stripUnknown: true });
+module.exports       = CreatePvcTask;
