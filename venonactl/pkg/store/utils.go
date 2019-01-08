f7cb664215a52693fd2c3502a41056978cb14d99
Oleg Sucharevich <olegs@codefresh.io>
2019-01-08 15:17:41 +0200

    calculate latest version from github release

--- venonactl/pkg/store/utils.go
+++ venonactl/pkg/store/utils.go
@@ -1,9 +1,31 @@
 package store
 
-const (
-	Default = "master"
+import (
+	"context"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/google/go-github/github"
 )
 
-func GetLatestVersion() string {
-	return Default
+const (
+	Default = "latest"
+)
+
+func getLatestVersion() string {
+	version := Default
+	client := github.NewClient(nil)
+	releases, _, err := client.Repositories.ListReleases(context.Background(), "codefresh-io", "venona", &github.ListOptions{})
+	if err != nil {
+		logrus.Errorf("Request to get latest version of venona been rejected , setting version to latest. Original error: %s", err.Error())
+		return version
+	}
+	for _, release := range releases {
+		name := strings.Split(*release.Name, "v")
+		if len(name) == 2 {
+			return name[1]
+		}
+	}
+	return version
 }
