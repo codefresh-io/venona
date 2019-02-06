f625b95a282c951c3fb9d7529dff2e4771d34671
Oleg Sucharevich <olegs@codefresh.io>
2019-02-06 19:44:31 +0200

    register all cloud provider to interact with kube api
    Signed-off-by: Oleg Sucharevich <olegs@codefresh.io>

--- venonactl/pkg/operators/helper.go
+++ venonactl/pkg/operators/helper.go
@@ -29,7 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
