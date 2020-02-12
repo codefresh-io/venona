89a7990d2e98ff863eefcbf0e122f200e27e8eb2
Fernando Carletti <contato@fernandocarletti.net>
2020-02-12 10:23:06 -0600

    Refactor tolerations arguments for the install command (#82)
    * feat: ignore idea files
    
    * chore: run go fmt
    
    * refactor: use k8s api for tolerations
    
    * refactor: tolerations arguments
    
    * chore: update install options in README
    
    * chore: bump version to 0.30.0

--- venonactl/hack/build.sh
+++ venonactl/hack/build.sh
@@ -2,6 +2,8 @@
 set -e
 OUTFILE=/usr/local/bin/venonactl
 go generate ${PWD}/hack/generate.go
+go fmt ${PWD}/pkg/obj/kubeobj/kubeobj.go
+go fmt ${PWD}/pkg/templates/kubernetes/templates.go
 go build -ldflags '-X github.com/codefresh-io/venona/venonactl/cmd.localDevFlow=true' -o $OUTFILE main.go
 
 chmod +x $OUTFILE
