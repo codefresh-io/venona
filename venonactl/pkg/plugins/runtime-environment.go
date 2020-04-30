c588fc2bdc1929045705669e3dd2d0f5a7c6c322
Oleg Sucharevich <oleg2807@gmail.com>
2020-04-30 10:38:12 +0300

    set runtime required memory to 1Gi

--- venonactl/pkg/plugins/runtime-environment.go
+++ venonactl/pkg/plugins/runtime-environment.go
@@ -89,7 +89,7 @@ func (u *runtimeEnvironmentPlugin) Install(opt *InstallOptions, v Values) (Value
 		res, err := ensureClusterRequirements(cs, validationRequest{
 			strictMode: true,
 			cpu:        "1",
-			momorySize: "2Gi",
+			momorySize: "1Gi",
 		})
 		if err != nil {
 			return nil, err
