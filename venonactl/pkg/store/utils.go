83383c32d6b2d07206640c4b998ee1081941a743
Oleg Sucharevich <olegs@codefresh.io>
2020-05-12 15:55:54 +0300

    return latest version in 0.x.x scope, ignore 1.x.x (#115)
    * return latest version in 0.x.x scope, ignore 1.x.x

--- venonactl/pkg/store/utils.go
+++ venonactl/pkg/store/utils.go
@@ -15,24 +15,51 @@ const (
 type (
 	logger interface {
 		Debug(string, ...interface{})
+		Error(string, ...interface{})
 	}
 )
 
+// GetLatestVersion return the latest version of 0.x.x
+// versions >= 1.0.0 are not compitable with 0.3.x and not installed using brew installation
+// therefore, GetLatestVersion will return latest version in 0.x.x space
 func GetLatestVersion(logger logger) string {
-	version := DefaultVersion
+	defaultversion := DefaultVersion
 	client := github.NewClient(nil)
 	releases, _, err := client.Repositories.ListReleases(context.Background(), "codefresh-io", "venona", &github.ListOptions{})
 	if err != nil {
-		logger.Debug("Request to get latest version of venona been rejected , setting version to latest. Original error: %s", err.Error())
-		return version
+		logger.Error("Request to get latest version of venona been rejected , setting version to latest. Original error: %s", err.Error())
+		return defaultversion
 	}
 	for _, release := range releases {
+		if release == nil {
+			continue
+		}
+		if release.Draft == nil {
+			continue
+		}
+		if *release.Draft {
+			continue
+		}
 		name := strings.Split(*release.Name, "v")
-		if len(name) == 2 {
-			return name[1]
+
+		if len(name) < 2 {
+			continue
+		}
+		latestAllowed, err := version.NewVersion("1.0.0")
+		if err != nil {
+			logger.Error("Failed to calc semver from version, setting version to latest. error: %s", err.Error())
+			return DefaultVersion
+		}
+		current, err := version.NewVersion(name[1])
+		if err != nil {
+			logger.Error("Failed to calc semver from version, setting version to latest. error: %s", err.Error())
+			return DefaultVersion
+		}
+		if current.LessThan(latestAllowed) {
+			return current.String()
 		}
 	}
-	return version
+	return defaultversion
 }
 
 func IsRunningLatestVersion() (bool, error) {
