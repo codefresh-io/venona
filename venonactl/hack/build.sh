ab7f27d9b5af54a9b8d6291252a3cda57badf1aa
Roi Kramer <roi.kramer@codefresh.io>
2020-06-02 16:56:26 +0300

    revert build.sh

--- venonactl/hack/build.sh
+++ venonactl/hack/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-OUTFILE=~/.codefresh/agent/venona
+OUTFILE=/usr/local/bin/venonactl
 go generate ${PWD}/hack/generate.go
 go fmt ${PWD}/pkg/obj/kubeobj/kubeobj.go
 go fmt ${PWD}/pkg/templates/kubernetes/templates.go
 VERSION="$(cat VERSION)-$(git rev-parse --short HEAD)"
 echo "Setting up version $VERSION"
-go build -ldflags "-X github.com/codefresh-io/venona/venonactl/cmd.version=$VERSION" -o $OUTFILE
+go build -ldflags "-X github.com/codefresh-io/venona/venonactl/cmd.version=$VERSION" -o $OUTFILE main.go
 
 chmod +x $OUTFILE
