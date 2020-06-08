98518d1cd1b6ac5c71a03f03ff68953bc79bcbff
Oleg Sucharevich <oleg2807@gmail.com>
2020-06-08 10:34:33 +0300

    add ci yaml

--- venona/cmd/root.go
+++ venona/cmd/root.go
@@ -1,18 +1,14 @@
 package cmd
 
 import (
-	"github.com/spf13/viper"
-
 	"github.com/spf13/cobra"
 )
 
-var cnf *viper.Viper = viper.New()
-
-var rootCmdOptions struct{}
+var version string
 
 var rootCmd = &cobra.Command{
 	Use:     "venona",
-	Version: "1.2.0", // TODO: read from ld flags
+	Version: version,
 	Long:    "Codefresh agent process",
 }
 
---
+++ venona/scripts/build.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+set -e
+
+VERSION=$(cat VERSION)
+
+echo "Building version $VERSION"
+
+go build -ldflags "-X github.com/codefresh-io/go/venona/cmd.version=$VERSION" -o venona *.go
