ba68514acb4875d5b66e17f9252de27323a9316e
Oren Gurfinkel <oren.gurfinkel@codefresh.io>
2020-06-08 14:08:58 +0300

    wip

--- venona/pkg/agent/agent_test.go
+++ venona/pkg/agent/agent_test.go
@@ -1,3 +1,17 @@
+// Copyright 2020 The Codefresh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package agent
 
 import (
@@ -51,19 +65,19 @@ func Test_groupTasks(t *testing.T) {
 						},
 					},
 				},
-			"2": {
-				{
-					Metadata: codefresh.Metadata{
-						Workflow: "2",
+				"2": {
+					{
+						Metadata: codefresh.Metadata{
+							Workflow: "2",
+						},
 					},
 				},
 			},
 		},
-	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			groupedTasks  := groupTasks(tt.args.tasks)
+			groupedTasks := groupTasks(tt.args.tasks)
 			assert.Equal(t, tt.want, groupedTasks)
 		})
 	}
