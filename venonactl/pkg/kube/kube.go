637086caadda872ece8f2035e3d8e381d292492b
Roi Kramer <roi.kramer@codefresh.io>
2020-06-24 16:39:15 +0300

    added incluster config fallback

--- venonactl/pkg/kube/kube.go
+++ venonactl/pkg/kube/kube.go
@@ -1,20 +1,20 @@
 package kube
 
 import (
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1Core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type (
 	Kube interface {
 		BuildClient() (*kubernetes.Clientset, error)
-		BuildConfig() (clientcmd.ClientConfig)
-		EnsureNamespaceExists(cs *kubernetes.Clientset) (error)
+		BuildConfig() clientcmd.ClientConfig
+		EnsureNamespaceExists(cs *kubernetes.Clientset) error
 	}
 
 	kube struct {
@@ -48,6 +48,9 @@ func (k *kube) BuildClient() (*kubernetes.Clientset, error) {
 		config, err = rest.InClusterConfig()
 	} else {
 		config, err = k.BuildConfig().ClientConfig()
+		if err != nil { // if cannot create from kubeConfigPath, try in-cluster config
+			config, err = rest.InClusterConfig()
+		}
 	}
 	if err != nil {
 		return nil, err
@@ -56,19 +59,19 @@ func (k *kube) BuildClient() (*kubernetes.Clientset, error) {
 	return cs, nil
 }
 
-func (k *kube) EnsureNamespaceExists(cs *kubernetes.Clientset) (error) {
+func (k *kube) EnsureNamespaceExists(cs *kubernetes.Clientset) error {
 	_, err := cs.CoreV1().Namespaces().Get(k.namespace, v1.GetOptions{})
-	if err != nil  {
-		nsSpec := &v1Core.Namespace{ObjectMeta: metav1.ObjectMeta {Name: k.namespace}};
+	if err != nil {
+		nsSpec := &v1Core.Namespace{ObjectMeta: metav1.ObjectMeta{Name: k.namespace}}
 		_, err := cs.CoreV1().Namespaces().Create(nsSpec)
 		if err != nil {
-			return  err
-		} 
+			return err
+		}
 	}
 	return nil
 }
 
-func (k *kube) BuildConfig() (clientcmd.ClientConfig) {
+func (k *kube) BuildConfig() clientcmd.ClientConfig {
 	config := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 		&clientcmd.ClientConfigLoadingRules{ExplicitPath: k.pathToKubeConfig},
 		&clientcmd.ConfigOverrides{
@@ -77,6 +80,6 @@ func (k *kube) BuildConfig() (clientcmd.ClientConfig) {
 				Namespace: k.namespace,
 			},
 		})
-		return config
+	return config
 
 }
