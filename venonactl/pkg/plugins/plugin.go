dd99286cb587f99f5aca839d854e94ebf3bc5973
roi-codefresh <60569147+roi-codefresh@users.noreply.github.com>
2020-07-01 08:39:23 +0300

    In cluster config (#167)

--- venonactl/pkg/plugins/plugin.go
+++ venonactl/pkg/plugins/plugin.go
@@ -8,7 +8,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
 )
 
 const (
@@ -59,7 +59,7 @@ type (
 		IsDefaultStorageClass bool
 		KubeBuilder           interface {
 			BuildClient() (*kubernetes.Clientset, error)
-			BuildConfig() clientcmd.ClientConfig
+			BuildConfig() (*rest.Config, error)
 			EnsureNamespaceExists(cs *kubernetes.Clientset) error
 		}
 		AgentKubeBuilder interface {
