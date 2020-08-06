0e123ba7fe616a7f184a26b768f8f38e41f12b5c
kosta709 <kosta777@gmail.com>
2020-08-06 23:16:20 +0300

    added debugging from codefresh-cli

--- venonactl/hack/build-linux.sh
+++ venonactl/hack/build-linux.sh
@@ -4,6 +4,7 @@ OUTFILE=$PWD/venonactl-linux
 go generate ${PWD}/hack/generate.go
 go fmt ${PWD}/pkg/obj/kubeobj/kubeobj.go
 go fmt ${PWD}/pkg/templates/kubernetes/templates.go
-GOOS=linux GOARCH=386 go build -ldflags '-X github.com/codefresh-io/venona/venonactl/cmd.localDevFlow=true' -o $OUTFILE main.go
+
+GOOS=linux  go build -gcflags=all="-N -l" -ldflags '-X github.com/codefresh-io/venona/venonactl/cmd.localDevFlow=true'  -o $OUTFILE main.go
 
 chmod +x $OUTFILE
