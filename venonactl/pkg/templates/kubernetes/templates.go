5c0ce4cb149f221a576256f70a2367869f375c74
Pavel Nosovets <pavel.nosovets@codefresh.io>
2020-08-17 17:16:36 +0300

    Updated template.go

--- venonactl/pkg/templates/kubernetes/templates.go
+++ venonactl/pkg/templates/kubernetes/templates.go
@@ -733,6 +733,10 @@ parameters:
   AvailabilityZone: {{ .Storage.AvailabilityZone }}
   # ext4 or xfs (default to ext4 )
   fsType: {{ .Storage.FsType | default "ext4" }}
+  # "true" or "false" (default - "false")
+  encrypted: {{ .Storage.Encrypted | default "false" }}
+  # KMS Key ID
+  kmsKeyId: {{ .Storage.KmsKeyId }}
 {{- end }}`
 
 	templatesMap["venonaconf.secret.venona.yaml"] = `apiVersion: v1
