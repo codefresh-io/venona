f606efac873ea44b8f82e115dd6631f113e457aa
Pavel Nosovets <56305887+palson-cf@users.noreply.github.com>
2020-08-25 10:06:41 +0300

    Bump volume provisioner (#178)

--- venonactl/pkg/store/store.go
+++ venonactl/pkg/store/store.go
@@ -132,7 +132,7 @@ func (s *Values) BuildValues() map[string]interface{} {
 			"AwsAccessKeyId":       "",
 			"AwsSecretAccessKey":   "",
 			"VolumeProvisioner": map[string]interface{}{
-				"Image":        "codefresh/dind-volume-provisioner:v23",
+				"Image":        "codefresh/dind-volume-provisioner:v24",
 				"NodeSelector": s.KubernetesAPI.NodeSelector,
 				"Tolerations":  s.KubernetesAPI.Tolerations,
 			},
