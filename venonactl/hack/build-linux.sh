a5cf0f09c5fb33bb79f6670dcd9353cab286f80b
kosta709 <kosta777@gmail.com>
2020-09-03 21:15:50 +0300

    fixed-windows build

--- venonactl/hack/build-linux.sh
+++ venonactl/hack/build-linux.sh
@@ -5,6 +5,6 @@ go generate ${PWD}/hack/generate.go
 go fmt ${PWD}/pkg/obj/kubeobj/kubeobj.go
 go fmt ${PWD}/pkg/templates/kubernetes/templates.go
 
-GOOS=linux  go build -gcflags=all="-N -l" -ldflags '-X github.com/codefresh-io/venona/venonactl/cmd.localDevFlow=true'  -o $OUTFILE main.go
+GOOS=linux  go build -gcflags=all="-N -l" -ldflags '-X github.com/codefresh-io/venona/venonactl/cmd.localDevFlow=true'  -o $OUTFILE .
 
 chmod +x $OUTFILE
