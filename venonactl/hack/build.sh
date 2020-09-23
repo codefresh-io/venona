103ce65addad1d81dc4a299e317add4646df1287
Roi Kramer <roi.kramer@codefresh.io>
2020-09-23 09:52:04 +0300

    bump

--- venonactl/hack/build.sh
+++ venonactl/hack/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-OUTFILE=~/.codefresh/agent/venona
+OUTFILE=/usr/local/bin/venonactl
 go generate ${PWD}/hack/generate.go
 go fmt ${PWD}/pkg/obj/kubeobj/kubeobj.go
 go fmt ${PWD}/pkg/templates/kubernetes/templates.go
