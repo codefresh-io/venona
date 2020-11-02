5cb68b8c9f2052970337bcbb0899fbc6e91a901a
roi-codefresh <60569147+roi-codefresh@users.noreply.github.com>
2020-11-02 11:48:27 +0200

    fixed windows compatiblity bugs and added app-proxy upgrade (#199)
    * fixed windows compatiblity bugs and added app-proxy upgrade
    
    * removed useless write to file
    
    Co-authored-by: roikramer120 <roikramer120@gmail.com>

--- venonactl/pkg/plugins/app-proxy.go
+++ venonactl/pkg/plugins/app-proxy.go
@@ -1,13 +1,28 @@
 package plugins
 
+/*
+Copyright 2019 The Codefresh Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/codefresh-io/venona/venonactl/pkg/logger"
 	templates "github.com/codefresh-io/venona/venonactl/pkg/templates/kubernetes"
 	"github.com/stretchr/objx"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type appProxyPlugin struct {
@@ -49,25 +64,7 @@ func (u *appProxyPlugin) Install(opt *InstallOptions, v Values) (Values, error) 
 	pathPrefix := objx.New(v["AppProxy"]).Get("PathPrefix").Str()
 	appProxyURL := fmt.Sprintf("https://%v%v", host, pathPrefix)
 	u.logger.Info(fmt.Sprintf("\napp proxy is running at: %v", appProxyURL))
-
-	// update IPC
-	file := os.NewFile(3, "pipe")
-	if file == nil {
-		return v, nil
-	}
-	data := map[string]interface{}{
-		"ingressIP": appProxyURL,
-	}
-	var jsonData []byte
-	jsonData, err = json.Marshal(data)
-	n, err := file.Write(jsonData)
-	if err != nil {
-		u.logger.Error("Failed to write to stream", err)
-		return v, fmt.Errorf("Failed to write to stream")
-	}
-	u.logger.Debug(fmt.Sprintf("%v bytes were written to stream\n", n))
-	return v, err
-
+	return v, nil
 }
 
 func (u *appProxyPlugin) Status(statusOpt *StatusOptions, v Values) ([][]string, error) {
@@ -93,7 +90,33 @@ func (u *appProxyPlugin) Delete(deleteOpt *DeleteOptions, v Values) error {
 }
 
 func (u *appProxyPlugin) Upgrade(opt *UpgradeOptions, v Values) (Values, error) {
-	return nil, nil
+	kubeClientset, err := opt.KubeBuilder.BuildClient()
+	if err != nil {
+		u.logger.Error(fmt.Sprintf("Cannot create kubernetes clientset: %v ", err))
+		return nil, err
+	}
+
+	list, err := kubeClientset.CoreV1().Pods(opt.ClusterNamespace).List(metav1.ListOptions{LabelSelector: fmt.Sprintf("app=%v", opt.Name)})
+	if err != nil {
+		u.logger.Error(fmt.Sprintf("Failed to list app-proxy pods: %v ", err))
+		return nil, err
+	}
+	if len(list.Items) == 0 {
+		u.logger.Info("no app-proxy pods found")
+		return nil, nil
+	}
+
+	for _, pod := range list.Items {
+		podName := pod.ObjectMeta.Name
+		u.logger.Debug(fmt.Sprintf("Deleting app-proxy pod: %v", podName))
+		err = kubeClientset.CoreV1().Pods(opt.ClusterNamespace).Delete(podName, &metav1.DeleteOptions{})
+		if err != nil {
+			u.logger.Error(fmt.Sprintf("Cannot delete app-proxy pod: %v ", err))
+			return nil, err
+		}
+	}
+
+	return v, nil
 }
 func (u *appProxyPlugin) Migrate(*MigrateOptions, Values) error {
 	return fmt.Errorf("not supported")
