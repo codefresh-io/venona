6c0d21eb24b7c891e1afca19880829d93e6871f7
roi.kramer <roi.kramer@codefresh.io>
2021-03-03 17:45:10 +0200

    rootless

--- venonactl/hack/build.sh
+++ venonactl/hack/build.sh
@@ -6,6 +6,6 @@ go fmt ${PWD}/pkg/obj/kubeobj/kubeobj.go
 go fmt ${PWD}/pkg/templates/kubernetes/templates.go
 VERSION="$(cat VERSION)-$(git rev-parse --short HEAD)"
 echo "Setting up version $VERSION"
-go build -ldflags "-X github.com/codefresh-io/venona/venonactl/cmd.version=CR-3159" -v -o $OUTFILE main.go
+go build -ldflags "-X github.com/codefresh-io/venona/venonactl/cmd.version=$VERSION" -v -o $OUTFILE main.go
 
 chmod +x $OUTFILE
