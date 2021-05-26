795d3e5ba66e2b46153776ad284c2e812e982eac
alex-codefresh <a.cheshko@codefresh.io>
2021-05-26 09:02:22 +0300

    Update the default volume provisioner image to 1.31.0 (CR-3578)

--- venonactl/pkg/store/store.go
+++ venonactl/pkg/store/store.go
@@ -174,7 +174,7 @@ func (s *Values) BuildValues() map[string]interface{} {
 			"AwsAccessKeyId":       "",
 			"AwsSecretAccessKey":   "",
 			"VolumeProvisioner": map[string]interface{}{
-				"Image":          "codefresh/dind-volume-provisioner:1.30.0",
+				"Image":          "codefresh/dind-volume-provisioner:1.31.0",
 				"NodeSelector":   s.KubernetesAPI.NodeSelector,
 				"Resources":      s.VolumeProvisioner.Resources,
 				"MountAzureJson": false,
