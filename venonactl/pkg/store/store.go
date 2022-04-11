d3aafbd32a67df05768c7949927f03fb6240543e
danielm-codefresh <daniel.maizel@codefresh.io>
2022-04-11 16:51:40 +0300

    update dind-volume-provisioner version

--- venonactl/pkg/store/store.go
+++ venonactl/pkg/store/store.go
@@ -174,7 +174,7 @@ func (s *Values) BuildValues() map[string]interface{} {
 			"AwsAccessKeyId":       "",
 			"AwsSecretAccessKey":   "",
 			"VolumeProvisioner": map[string]interface{}{
-				"Image":          "codefresh/dind-volume-provisioner:1.31.5",
+				"Image":          "codefresh/dind-volume-provisioner:1.31.6",
 				"NodeSelector":   s.KubernetesAPI.NodeSelector,
 				"Resources":      s.VolumeProvisioner.Resources,
 				"MountAzureJson": false,
