0018a6b1aae4230d73804e53cbdaf92b4dbf9bee
roi.kramer <roi.kramer@codefresh.io>
2023-03-26 12:30:07 +0300

    wip

--- venonactl/hack/build.sh
+++ venonactl/hack/build.sh
@@ -4,7 +4,7 @@ OUTFILE=/usr/local/bin/venonactl
 go generate ${PWD}/hack/generate.go
 go fmt ${PWD}/pkg/obj/kubeobj/kubeobj.go
 go fmt ${PWD}/pkg/templates/kubernetes/templates.go
-VERSION="$(cat VERSION)"
+VERSION="$(cat VERSION)-$(git rev-parse --short HEAD)"
 echo "Setting up version $VERSION"
 CGO_ENABLED=0 go build -ldflags "-X github.com/codefresh-io/venona/venonactl/cmd.version=$VERSION" -v -o $OUTFILE main.go
 
--- venonactl/pkg/store/store.go
+++ venonactl/pkg/store/store.go
@@ -174,7 +174,7 @@ func (s *Values) BuildValues() map[string]interface{} {
 			"AwsAccessKeyId":       "",
 			"AwsSecretAccessKey":   "",
 			"VolumeProvisioner": map[string]interface{}{
-				"Image":          "codefresh/dind-volume-provisioner:1.33.1",
+				"Image":          "codefresh/dind-volume-provisioner:1.33.2",
 				"NodeSelector":   s.KubernetesAPI.NodeSelector,
 				"Resources":      s.VolumeProvisioner.Resources,
 				"MountAzureJson": false,
