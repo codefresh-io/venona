44f0b8977dfc3f329c8c29c0ea9d07b0b77175af
roi-codefresh <roi.kramer@codefresh.io>
2023-03-29 11:52:02 +0300

    Bump vp 1.33.3 (#341)
    * bumped volume provisioner to 1.33.3
    
    * chart version bump
    
    * docs
    
    * empty
    
    * override alpine
    
    ---------
    
    Co-authored-by: mikhail-klimko <mikhail.klimko@codefresh.io>

--- .deploy/scripts/get-all-images.sh
+++ .deploy/scripts/get-all-images.sh
@@ -3,4 +3,4 @@ CHARTDIR="${MYDIR}/../cf-runtime"
 VALUESFILE="../cf-runtime/.ci/values-ci.yaml"
 OUTPUTFILE=$1
 helm dependency update $CHARTDIR
-helm template --values $VALUESFILE $CHARTDIR | grep image: | awk -F 'image:' '{print $2}' | tr -d '"' | uniq > $OUTPUTFILE
+helm template --values $VALUESFILE $CHARTDIR | grep -E 'image:' | awk -F ': ' '{print $2}' | tr -d '"' | uniq > $OUTPUTFILE
