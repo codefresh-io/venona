9a87b8f918733b568aeae6587d91a4dc58edb346
Noam Gal <noam.gal@codefresh.io>
2023-06-29 09:23:54 +0300

    switch to buffered queue
    added logs after finish workflow handle

--- venona/pkg/queue/queue.go
+++ venona/pkg/queue/queue.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"errors"
 	"sync"
+	"time"
 
 	"github.com/codefresh-io/go/venona/pkg/logger"
 	"github.com/codefresh-io/go/venona/pkg/monitoring"
@@ -52,7 +53,7 @@ func New(runtimes map[string]runtime.Runtime, log logger.Logger, wg *sync.WaitGr
 		log:         log,
 		wg:          wg,
 		monitor:     monitor,
-		queue:       make(chan *workflow.Workflow),
+		queue:       make(chan *workflow.Workflow, 1000),
 		concurrency: concurrency,
 		stop:        make([]chan bool, concurrency),
 	}
@@ -93,7 +94,10 @@ func (wfq *WorkflowQueue) handleChannel(ctx context.Context, stopChan chan bool,
 			ctxCancelled = true
 		case wf := <-wfq.queue:
 			wfq.log.Info("handling workflow", "handlerId", id, "workflow", wf.Metadata.Workflow)
+			start := time.Now()
 			wfq.handleWorkflow(ctx, wf)
+			end := time.Now()
+			wfq.log.Info("done handling workflow", "handlerId", id, "workflow", wf.Metadata.Workflow, "duration", end.Sub(start))
 		default:
 			if ctxCancelled {
 				wfq.log.Info("stopped workflow handler", "handlerId", id)
