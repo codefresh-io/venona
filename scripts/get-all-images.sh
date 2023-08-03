f0a20303ab38e6e18e40b952060f7a0e69a82ddb
mikhail-klimko <mikhail.klimko@codefresh.io>
2023-08-03 15:45:06 +0300

    fix: get-all-images script

--- scripts/get-all-images.sh
+++ scripts/get-all-images.sh
@@ -3,4 +3,4 @@ CHARTDIR="${MYDIR}/../charts/cf-runtime"
 VALUESFILE="../charts/cf-runtime/.ci/values-ci.yaml"
 OUTPUTFILE=$1
 helm dependency update $CHARTDIR
-helm template --values $VALUESFILE $CHARTDIR | grep -E 'image: | dindImage:' | awk -F ': ' '{print $2}' | tr -d '"' | uniq > $OUTPUTFILE
+helm template --values $VALUESFILE --set global.runtimeName="dummy" $CHARTDIR | grep -E 'image: | dindImage:' | awk -F ': ' '{print $2}' | tr -d '"' | uniq > $OUTPUTFILE
