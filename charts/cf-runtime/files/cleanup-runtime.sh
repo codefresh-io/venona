186270754f87974847aab1c5d6d567fdcb35f1d8
mikhail-klimko <mikhail.klimko@codefresh.io>
2023-09-25 15:38:27 +0300

    fix: delete agent and dind secrets (#397)

--- charts/cf-runtime/files/cleanup-runtime.sh
+++ charts/cf-runtime/files/cleanup-runtime.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "-----"
-echo "API_HOST:         ${API_HOST}"
-echo "AGENT_NAME:       ${AGENT_NAME}"
-echo "RUNTIME_NAME:     ${RUNTIME_NAME}"
-echo "AGENT:            ${AGENT}"
+echo "API_HOST:           ${API_HOST}"
+echo "AGENT_NAME:         ${AGENT_NAME}"
+echo "RUNTIME_NAME:       ${RUNTIME_NAME}"
+echo "AGENT:              ${AGENT}"
+echo "AGENT_SECRET_NAME:  ${AGENT_SECRET_NAME}"
+echo "DIND_SECRET_NAME:   ${DIND_SECRET_NAME}"
 echo "-----"
 
 auth() {
@@ -23,11 +25,13 @@ remove_agent() {
   codefresh delete agent ${AGENT_NAME} || true
 }
 
-remove_finalizers() {
+remove_secrets() {
   kubectl patch secret $(kubectl get secret -l codefresh.io/internal=true | awk 'NR>1{print $1}' | xargs) -p '{"metadata":{"finalizers":null}}' --type=merge || true
+  kubectl delete secret $AGENT_SECRET_NAME || true
+  kubectl delete secret $DIND_SECRET_NAME || true
 }
 
 auth
 remove_runtime
 remove_agent
-remove_finalizers
+remove_secrets
