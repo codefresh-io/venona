f52d52d7213604a929308a95680a251fe5746409
Mikhail Klimko <mikhail.klimko@octopus.com>
2024-11-05 17:59:00 +0300

    feat: image digest

--- charts/cf-runtime/.ci/image-digests.sh
+++ charts/cf-runtime/.ci/image-digests.sh
@@ -8,7 +8,5 @@ docker run \
     -v "$REPO_ROOT:/venona" \
     -u $(id -u) \
     --rm \
-    --entrypoint /bin/sh \
     quay.io/codefresh/codefresh-shell:0.0.20 \
-    -c \
-    /venona/scripts/update_values_with_digests.sh
+    /bin/bash /venona/scripts/update_values_with_digests.sh
