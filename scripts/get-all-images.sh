b8a52bb6d8c26af967e4f4054d99befdba0091d0
Mikhail Klimko <mikhail.klimko@octopus.com>
2024-11-19 12:36:06 +0300

    fix: sort images in get-all-images script

--- scripts/get-all-images.sh
+++ scripts/get-all-images.sh
@@ -3,6 +3,6 @@ CHARTDIR="${MYDIR}/../charts/cf-runtime"
 VALUESFILE="${MYDIR}/../charts/cf-runtime/.ci/values-ci.yaml"
 OUTPUTFILE=$1
 helm dependency update $CHARTDIR
-helm template --values $VALUESFILE --set global.runtimeName="dummy" $CHARTDIR | grep -E 'image: | dindImage:' | awk -F ': ' '{print $2}' | tr -d '"' | tr -d "'" | uniq > $OUTPUTFILE
+helm template --values $VALUESFILE --set global.runtimeName="dummy" $CHARTDIR | grep -E 'image: | dindImage:' | awk -F ': ' '{print $2}' | tr -d '"' | tr -d "'" | sort | uniq > $OUTPUTFILE
 
 cat $OUTPUTFILE | tr '@' '\n' | awk 'NR % 2 == 1' | tee $OUTPUTFILE
