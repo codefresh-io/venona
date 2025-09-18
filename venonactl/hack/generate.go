ce55afdc65772104e70b583521453e419432f9ea
Zhenya Tikhonov <zhenya.tikhonov@octopus.com>
2025-09-18 18:47:06 +0400

    build: deprecate `venonactl` (#622)

--- venonactl/hack/generate.go
+++
@@ -1,7 +0,0 @@
-package hack
-
-/*
-We are using generated template.go for serialized kubernetes assets
-*/
-//go:generate go run github.com/codefresh-io/venona/venonactl/pkg/templates kubernetes
-//go:generate go run github.com/codefresh-io/venona/venonactl/pkg/obj kubernetes
