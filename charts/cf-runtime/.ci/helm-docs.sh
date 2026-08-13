bb6755d0ea3ed25a980a11a40bcc40830548d42a
Mikhail Klimko <mikhail.klimko@octopus.com>
2026-08-13 22:39:59 +0400

    update helm-docs.sh

--- charts/cf-runtime/.ci/helm-docs.sh
+++ charts/cf-runtime/.ci/helm-docs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 ## Reference: https://github.com/norwoodj/helm-docs
-set -eux
-REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+set -x
+REPO_ROOT="${1:-$(cd "$(dirname "$0")/../../.." && pwd)}"
+WORKDIR="${2}"
+
 echo "$REPO_ROOT"
+echo "$WORKDIR"
 
 echo "Running Helm-Docs"
 docker run \
     -v "$REPO_ROOT:/helm-docs" \
+    -w "/helm-docs/$WORKDIR" \
     -u $(id -u) \
     --rm \
     --entrypoint /bin/sh \
